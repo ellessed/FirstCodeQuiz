121ce8b4083cad41ae7f941288b7b2bba888c1ed
elleserdonaldson <ellessedon@gmail.com>
2022-10-22 23:30:57 +0100

    code quiz questions and answers added

---
+++ assets/questions.js
@@ -0,0 +1,43 @@
+// list of questions, choices and answers
+var questions = [
+  {
+    subject: "What does HTML stand for",
+    options: [
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyperlinks and Text Markup Language",
+    ],
+    answers: "Hyper Text Markup Language",
+  },
+  {
+    subject: "What does CSS stand for",
+    options: [
+      "Creators Style Sheets",
+      "Computer Style Sheets",
+      "Cascading Style Sheets",
+      "Colorful Style Sheet",
+    ],
+    answers: "Cascading Style Sheets",
+  },
+  {
+    subject: "Arrays in JavaScript can be used to store ___.",
+    options: [
+      "other arrays",
+      "numbers and strings",
+      "booleans",
+      "all of the above",
+    ],
+    answers: "all of the above",
+  },
+  {
+    subject: "Which of the following is not a programming language",
+    options: ["Java", "Python", "TypeScript", "Anaconda"],
+    answers: "Anaconda",
+  },
+  {
+    subject:
+      "A useful tool used during development and debugging for printing content to the debugger is: ",
+    options: ["Local Storage", "JavaScript", "Console.log", "For loops"],
+    answers: "Console.log",
+  },
+];
