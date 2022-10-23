5be7f73cb19ba7c44274fc1f8129d7dbe705e0b6
elleserdonaldson <ellessedon@gmail.com>
2022-10-23 17:27:39 +0100

    created sepearte script in javascript for the scores

---
+++ scores.js
@@ -0,0 +1,6 @@
+var scoresheet = document.getElementById("scoresheet");
+var backtoQuiz = document.getElementById("backtoQuiz");
+
+function onBackToQuiz() {
+  window.location.href = "index.html";
+}
