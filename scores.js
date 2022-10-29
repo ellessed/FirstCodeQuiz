2f74d318843a414a1e45c7151f262c48faa5f2ce
elleserdonaldson <ellessedon@gmail.com>
2022-10-29 10:42:11 +0100

    for loop

--- scores.js
+++ scores.js
@@ -4,3 +4,15 @@ var backtoQuiz = document.getElementById("backtoQuiz");
 function onBackToQuiz() {
   window.location.href = "index.html";
 }
+
+for (var i = 0; i < localStorage.length; i++) {
+  var initals = localStorage.key(i);
+  var score = localStorage.getItem(initals);
+  var result = document.createElement("div");
+  result.classList.add("result");
+  result.innerHTML = `<div class="score-item">${initals}</div>
+  <div class="score-item">${score}</div>`;
+  scoresheet.appendChild(result);
+}
+
+backtoQuiz.addEventListener("click", onBackToQuiz);
