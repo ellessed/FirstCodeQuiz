157716d8499990f6eb5fcecec63a89ba1c23cdcd
elleserdonaldson <ellessedon@gmail.com>
2023-03-19 14:25:19 +0000

    created timeout variable, worked on begingame functionality, fixed the issue where timer doesnt reduce depending on answers, users able to add name which is saved to local storage

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,96 +1,39 @@
+// Returns an Element object which represents the element whose id property matches the specified string
 var beginQuiz = document.getElementById("beginQuiz");
 var saveScore = document.getElementById("saveScore");
 var viewScores = document.getElementById("viewScores");
 var playAgain = document.getElementById("playAgain");
 
-var greeting = document.getElementById("greetig");
-var codeQuiz = document.getElementById("code-quiz");
+var greeting = document.getElementById("greeting");
+var quiz = document.getElementById("quiz");
 var end = document.getElementById("end");
 
 var options = document.getElementById("options");
 var message = document.getElementById("message");
 
 var timer = document.getElementById("timer");
+
 var summary = document.getElementById("summary");
 
 var secondsLeft = 0;
 var score = 0;
 var presentQuestion = 0;
 var countdownTimer;
+var Timeout;
 
-//  Stop the Countdown Timer
-// Clear the timer
-// Hide the question and display the result
-// Show the Score
-function endGame() {
-  clearfInterval(countdownTimer);
-  timer.textContent = "";
-  codeQuiz.style.display = "none";
-  end.style.display = "flex";
-  summary.textContent = "Your score is" + score;
-}
-
-function onSaveScore(e) {
-  var initials = document.getElementById("initials").value;
-  if (initials !== "") {
-    localStorage.setItem(initials, score);
-    document.getElementById("initals").value = "";
-  }
-}
-function onViewScores(e) {
-  window.location.href = "scores.html";
-}
-
-function onSelectAnswer(e) {
-  var rightAnswer = questions[presentQuestion].answers;
-  var userAnswer = e.target.textContent;
-  console.log(rightAnswer);
-  console.log(userAnswer);
-  if (rightAnswer === userAnswer) {
-    score++;
-    displayMessage("Correct");
-  } else {
-    score--;
-    displayMessage("Incorrect");
-    secondsLeft -= 5;
-  }
-
-  // Call up the next question
-  displayQuestion();
-}
-// Show message and clear the message after 2seconds
-function displayMessage(msg) {
-  message.textContent = msg;
-  setTimeout(function () {
-    message.textContent = " ";
-  }, 2000);
-}
-// Increment to get the next question
-function displayQuestion() {
-  console.log("current question is" + presentQuestion);
-  if (presentQuestion >= questions.length) {
-    stopGame();
-    return;
-  }
-  var question = questions[presentQuestion];
-  document.getElementById("question").textContent = question.subject;
-  console.log(question.subject);
-  options.innerHTML = "";
-  for (var i = 0; i < question.options.length; i++) {
-    var option = document.createElement("div");
-    option.textContent = question.options[i];
-    option.onclick = onSelectAnswer;
-    option.classList.add("option");
-
-    options.appendChild(option);
-  }
-  presentQuestion++;
-}
-
-function onStartGame() {
-  secondsLeft = 75;
+function beginGame() {
+  secondsLeft = 50;
   presentQuestion = 0;
   score = 0;
+
+  timer.textContent = secondsLeft;
+  timer.classList.remove("hidden");
+  beginQuiz.classList.add("hidden");
+
+  quiz.style.display = "block";
+  end.style.display = "none";
+
+  clearInterval(countdownTimer);
   countdownTimer = setInterval(function () {
     if (secondsLeft > 0) {
       timer.textContent = secondsLeft;
@@ -98,16 +41,111 @@ function onStartGame() {
       stopGame();
     }
     secondsLeft--;
-  }, 2000);
+  }, 1000);
 
-  document.getElementById("greeting").style.display = "none";
-  end.style.display = "none";
-  codeQuiz.style.display = "flex";
-
-  displayQuestion();
+  showQuestion();
 }
 
-beginQuiz.addEventListener("click", onStartGame);
+function showQuestion() {
+  if (presentQuestion >= questions.length) {
+    endGame();
+    return;
+  }
+
+  var question = questions[presentQuestion];
+  document.getElementById("question").textContent = question.subject;
+
+  options.innerHTML = "";
+
+  for (var i = 0; i < question.choices.length; i++) {
+    var option = document.createElement("button");
+    option.textContent = question.choices[i];
+    option.onclick = onSelectAnswer;
+    option.classList.add("option");
+
+    options.appendChild(option);
+  }
+}
+
+function showMessage(msg, options) {
+  message.textContent = msg;
+  message.classList.remove("visually-hidden");
+  message.classList.remove("correct");
+
+  if (options.isCorrect) {
+    message.classList.add("correct");
+  }
+
+  clearTimeout(Timeout);
+  Timeout = setTimeout(function () {
+    message.textContent = "Waiting for answer";
+    message.classList.add("visually-hidden");
+  }, 2000);
+}
+
+function showMessage(msg, options) {
+  message.textContent = msg;
+  message.classList.remove("visually-hidden");
+  message.classList.remove("correct");
+
+  if (options.isCorrect) {
+    message.classList.add("correct");
+  }
+
+  clearTimeout(Timeout);
+  Timeout = setTimeout(function () {
+    message.textContent = "Waiting for answer";
+    message.classList.add("visually-hidden");
+  }, 2000);
+}
+
+function onSelectAnswer(e) {
+  var correctAnswer = questions[presentQuestion].answer;
+  var usersAnswer = e.target.textContent;
+  //if the correct answer is equal to what the user has entered then the message pops up correct
+  //else if it is not equal to, time is deducte and incorrect message is displayed
+  if (correctAnswer === usersAnswer) {
+    score++;
+    showMessage("Correct!", { isCorrect: true });
+  } else {
+    score--;
+    secondsLeft -= 5;
+    showMessage("Incorrect", { isCorrect: false });
+  }
+  presentQuestion++;
+
+  showQuestion();
+}
+function onviewScores(e) {
+  window.location.href = "scores.html";
+}
+
+// returns the value of initials
+function onSaveScore(e) {
+  var initials = document.getElementById("initials").value;
+
+  if (initials !== "") {
+    localStorage.setItem(initials, score);
+
+    document.getElementById("initials").value = "";
+
+    window.location.href = "scores.html";
+  }
+}
+
+//once the game has ended, time is cleared and score is defined
+function endGame() {
+  clearInterval(countdownTimer);
+  timer.textContent = "";
+  timer.classList.add("hidden");
+
+  quiz.style.display = "none";
+  end.style.display = "block";
+
+  summary.textContent = "Your Score Is: " + score;
+}
+
+beginQuiz.addEventListener("click", beginGame);
 saveScore.addEventListener("click", onSaveScore);
-viewScores.addEventListener("click", onViewScores);
-playAgain.addEventListener("click", onStartGame);
+viewScores.addEventListener("click", onviewScores);
+playAgain.addEventListener("click", beginGame);
